2999ccecce944699966376a98662848746b1400c
LilianMShen <lilian.m.shen@gmail.com>
2022-11-15 17:32:58 -0600

    testing out some firebase stuff

--- app/src/main/java/com/example/memail/HomeActivity.java
+++ app/src/main/java/com/example/memail/HomeActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.*;
+
 public class HomeActivity extends AppCompatActivity {
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
 
+        CollectionReference templates = db.collection("Templates");
+
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("Category", "Organization");
+//        data.put("Draft", "Test 2");
+//        data.put("Title", "Draft");
+//        data.put("Format", "Default Draft");
+//        data.put("Topic", "Topic 2");
+//        templates.document().set(data);
+    }
 
 }
