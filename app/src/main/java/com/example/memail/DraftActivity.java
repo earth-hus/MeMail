5aeaccff3b22ad48b3f67fd8b42b6e6543ed4ee3
LilianMShen <lilian.m.shen@gmail.com>
2022-11-17 13:18:58 -0600

    SAVED IS WORKING!!

--- app/src/main/java/com/example/memail/DraftActivity.java
+++ app/src/main/java/com/example/memail/DraftActivity.java
@@ -15,16 +15,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DraftActivity extends AppCompatActivity {
-    ImageButton saveButton = findViewById(R.id.saveButton);
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    CollectionReference saved =db.collection("Saved");
-    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    ImageButton saveButton;
+    FirebaseFirestore db;
+    CollectionReference saved;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draft);
 
+        saveButton = findViewById(R.id.saveButton);
+        db = FirebaseFirestore.getInstance();
+        saved = db.collection("Saved");
+        mAuth = FirebaseAuth.getInstance();
 
         saveButton.setOnClickListener(this::onClick);
     }
@@ -33,9 +37,10 @@ public class DraftActivity extends AppCompatActivity {
         Map<String, Object> data = new HashMap<>();
         EditText draft = findViewById(R.id.draft);
         EditText title = findViewById(R.id.draftTitle);
-        data.put("Draft", draft.toString());
-        data.put("Title", title.toString());
+        data.put("Draft", draft.getText().toString());
+        data.put("Title", title.getText().toString());
         data.put("UID", mAuth.getUid());
+        saved.document().set(data);
     }
 
 
