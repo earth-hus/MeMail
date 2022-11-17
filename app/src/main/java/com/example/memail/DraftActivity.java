f048cc01a93c71a617b5ec576604a558c8f803e5
LilianMShen <lilian.m.shen@gmail.com>
2022-11-17 13:07:37 -0600

    updated path so topic goes to draft. updated draft so save button will save currentn draft in the database :)

--- app/src/main/java/com/example/memail/DraftActivity.java
+++ app/src/main/java/com/example/memail/DraftActivity.java
@@ -3,12 +3,40 @@ package com.example.memail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DraftActivity extends AppCompatActivity {
+    ImageButton saveButton = findViewById(R.id.saveButton);
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference saved =db.collection("Saved");
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draft);
+
+
+        saveButton.setOnClickListener(this::onClick);
     }
+
+    public void onClick(View v) {
+        Map<String, Object> data = new HashMap<>();
+        EditText draft = findViewById(R.id.draft);
+        EditText title = findViewById(R.id.draftTitle);
+        data.put("Draft", draft.toString());
+        data.put("Title", title.toString());
+        data.put("UID", mAuth.getUid());
+    }
+
+
 }
