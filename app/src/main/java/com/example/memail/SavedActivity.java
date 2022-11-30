8dea6559f7a71411f371750504f43ffd6b501429
Naman Jain <njain34@uic.edu>
2022-11-29 18:10:03 -0600

    progress

--- app/src/main/java/com/example/memail/SavedActivity.java
+++ app/src/main/java/com/example/memail/SavedActivity.java
@@ -3,54 +3,40 @@ package com.example.memail;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class FormatActivity extends AppCompatActivity {
-    // List of formats
-    ArrayList<String> templateList;
+public class SavedActivity extends AppCompatActivity {
 
-    // Database
+    ArrayList<String> list;
     FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_format);
-        getSupportActionBar().setTitle("Email Templates");
+        setContentView(R.layout.activity_saved);
+
+        db = FirebaseFirestore.getInstance();
+        getSupportActionBar().setTitle("Saved Drafts");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Get the topic of intended email
-        Bundle extras = getIntent().getExtras();
-        String topic = extras.getString("Topic");
-        String category = extras.getString("Category");
+        list = new ArrayList<>();
 
-        // Get instance of firebase firestore
-        db = FirebaseFirestore.getInstance();
-        templateList = new ArrayList<>();
-
-        ListView listView = (ListView) findViewById(R.id.format_list);
-
-        MyCustomAdapter adapter = new MyCustomAdapter(templateList, this,"FormatActivity");
+        ListView listView = (ListView) findViewById(R.id.Saved_list);
+        MyCustomAdapter adapter = new MyCustomAdapter(list, this,"FormatActivity",);
         listView.setAdapter(adapter);
 
-        db.collection("Templates").get()
+        db.collection("Saved").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
@@ -60,29 +46,17 @@ public class FormatActivity extends AppCompatActivity {
                                 Templates c = d.toObject(Templates.class);
 
                                 // Check to make sure topic matches
-                                if (topic.equals(c.getTopic()) && category.equals(c.getCategory())) {
-                                    templateList.add(c.getTitle());
+                                    list.add(c.getTitle());
                                     adapter.notifyDataSetChanged();
-                                }
                             }
                         } else {
-                            Toast.makeText(FormatActivity.this, "No templates found in Database", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SavedActivity.this, "No templates found in Database", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(FormatActivity.this, "Fail to get the data.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SavedActivity.this, "Fail to get the data.", Toast.LENGTH_SHORT).show();
                     }
-        });
-
-        System.out.println("test");
-
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item){
-        Intent myIntent = new Intent(getApplicationContext(), TopicActivity.class);
-        startActivity(myIntent);
-        return true;
     }
 }
